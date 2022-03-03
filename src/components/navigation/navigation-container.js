656bbfa79e3dde3bfc2e6d64a2d6f023f4f0cbe4
oscarbravo96 <96oscarbravo@gmail.com>
2022-03-03 16:34:00 -0500

    added initial navigation styling

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom"
+
+export default class NavigationContainer extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="nav-wrapper">
+                <div className="left-side">
+                    <NavLink exact to="/" activeClassName="nav-link-active">
+                        Home
+                    </NavLink>
+
+                    <NavLink to="/about-me" activeClassName="nav-link-active">
+                        About
+                    </NavLink>
+
+                    <NavLink to="/contact" activeClassName="nav-link-active">
+                        Contact
+                    </NavLink>
+
+                    <NavLink to="/blog" activeClassName="nav-link-active">
+                        Blog
+                    </NavLink>
+
+                    {false ? <button>Add Blog</button> : null}
+                </div>
+                <div className="right-side">
+                    Oscar Bravo
+                </div>
+            </div>
+        );
+    }
+}
