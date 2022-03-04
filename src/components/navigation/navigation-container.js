32704c819bf1c66e7114a715e056a118acc5fdf4
oscarbravo96 <96oscarbravo@gmail.com>
2022-03-04 16:19:02 -0500

    added mixins and buttons styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -10,23 +10,31 @@ export default class NavigationContainer extends Component {
         return (
             <div className="nav-wrapper">
                 <div className="left-side">
-                    <NavLink exact to="/" activeClassName="nav-link-active">
-                        Home
-                    </NavLink>
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to="/" activeClassName="nav-link-active">
+                            Home
+                        </NavLink>
+                    </div>
 
-                    <NavLink to="/about-me" activeClassName="nav-link-active">
-                        About
-                    </NavLink>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/about-me" activeClassName="nav-link-active">
+                            About
+                        </NavLink>
+                    </div>
 
-                    <NavLink to="/contact" activeClassName="nav-link-active">
-                        Contact
-                    </NavLink>
 
-                    <NavLink to="/blog" activeClassName="nav-link-active">
-                        Blog
-                    </NavLink>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/contact" activeClassName="nav-link-active">
+                            Contact
+                        </NavLink>
+                    </div>
 
-                    {false ? <button>Add Blog</button> : null}
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/blog" activeClassName="nav-link-active">
+                            Blog
+                        </NavLink>
+                    </div>
+
                 </div>
                 <div className="right-side">
                     Oscar Bravo
