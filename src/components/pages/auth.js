b742d7e83b650ca0ea31fbe2bfde946b046ac9c0
oscarbravo96 <96oscarbravo@gmail.com>
2022-03-15 04:32:57 -0400

    preparing to push to heroku

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react'
-import loginImg from "../../../static/assets/images/auth/login.jpg"
+import React, { Component } from "react";
+import Login from "../auth/login";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
 
 export default class Auth extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+    this.handleUnsuccessfulAuth = this.handleUnsuccessfulAuth.bind(this);
+  }
+
+  handleSuccessfulAuth() {
+    this.props.handleSuccessfulLogin();
+    this.props.history.push("/");
+  }
+
+  handleUnsuccessfulAuth() {
+    this.props.handleUnsuccessfulLogin();
+  }
+
   render() {
     return (
-      <div className='auth-page-wrapper'>
-          <div className='left-columnd' 
-            style={{
-                backgroundImage: `url(${loginImg})`
-            }}
-          />
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`
+          }}
+        />
 
-          <div className='right-column'>
-              <h1>login component</h1>
-          </div>
+        <div className="right-column">
+          <Login
+            handleSuccessfulAuth={this.handleSuccessfulAuth}
+            handleUnsuccessfulAuth={this.handleUnsuccessfulAuth}
+          />
+        </div>
       </div>
     );
   }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/bio/red-and-pika.png";
 
 export default function() {
   return (
-    <div>Contact</div>
-  )
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">229-338-9993</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">96oscarbravo@gmail.com</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Albany, GA</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
